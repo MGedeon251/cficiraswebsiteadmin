cefcaff54c5e7a583de0749cc549d3706178288a
Mardochet G. LEKOUNDA <gedeon.lekounda@gmail.com>
2023-05-07 12:11:02 +0100

    views/portal

--- server/routes/routes.users.js
+++ server/routes/routes.users.js
@@ -15,4 +15,39 @@ app.get('/login', (req, res)=> {
     res.render('auth-view/login');
 });
 
+app.get('/etudiant', (req, res)=>{
+    res.render('portal/etudiant');
+})
+
+app.get('/modules', (req, res)=>{
+    res.render('portal/modules');
+})
+
+app.get('/examens', (req, res)=>{
+    res.render('portal/examen');
+})
+
+app.get('/parcours', (req, res)=>{
+    res.render('portal/parcours');
+})
+
+app.get('/notes', (req, res)=>{
+    res.render('portal/notes');
+})
+
+app.get('/schedule', (req, res)=>{
+    res.render('portal/schedule');
+})
+
+app.get('/users', (req, res)=>{
+    res.render('portal/users');
+})
+
+app.get('/inscription', (req, res)=>{
+    res.render('portal/inscription');
+})
+app.get('/supcours', (req, res)=>{
+    res.render('portal/spcours');
+})
+
 module.exports = app ;
